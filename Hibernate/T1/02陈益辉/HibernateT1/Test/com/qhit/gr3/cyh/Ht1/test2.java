507deab0d743d216eed0f5c9f485b421d20e3927
1640506509 <1640506509@qq.com>
2017-12-11 15:01:28 +0800

    陈益辉提交HibernateT1作业并修改SBM文件路径

---
+++ Hibernate/T1/02陈益辉/HibernateT1/Test/com/qhit/gr3/cyh/Ht1/test2.java
@@ -0,0 +1,14 @@
+package com.qhit.gr3.cyh.Ht1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
